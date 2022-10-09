2518389db02c15715d3abece1e09873a27b37759
MuhammadUmerWasi <87394051+MuhammadUmerWasi@users.noreply.github.com>
2022-10-09 12:22:01 +0500

    Route change

--- Routes/Route.js
+++ Routes/Route.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const custom = require('../Middleware/Middle.js')
+const staticPath = path.join(__dirname,'../Templates');
+
+router.use(express.static(staticPath));
 
 //router.use(custom.auth)
 //router.use(custom.admin)
 
 router.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../Templates/home/home.html'))
+})
+
+
+router.get('/register',(req,res) => {
+    res.sendFile(path.join(__dirname,'../Templates/register/register.html'))
 })
 
 router.get('/login',(req,res) => {
@@ -35,5 +43,8 @@ router.get('/semester',custom.auth,(req,res) => {
 })
 
 
+router.get('/payment',custom.auth,(req,res) => {
+    res.sendFile(path.join(__dirname,'../Templates/payment/payment.html'))
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.Port || 3000;
+const port = process.env.Port || 4000;
 
 app.use('/',require(path.join(__dirname,'Routes/Route.js')));
 
 app.listen(port, () => {
-    console.log('Express listening on port http://localhost:3000');
+    console.log('Express listening on port http://localhost:4000');
 });
